f1e2f834c1a034ef10a148bb9419b7af0c73afe1
Joshua Cole <j_cole_356@yahoo.com>
2015-04-30 13:06:07 -0700

    Add piece moving functionality

--- w2d4/checkers/board.rb
+++ w2d4/checkers/board.rb
@@ -1,3 +1,51 @@
 class Board
 
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+
+
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos)
+    x, y = pos
+    @grid[x, y] = @grid[x][y]
+  end
+
+  def dup_board
+
+  end
+
+  def render
+
+  end
+
+  def seed_board
+    8.times do |row|
+      next if row.between?(3, 4)
+      color = row.between?(0, 3) ? :red : :black
+      8.times do |i|
+        if row.even?
+          next if i.even?
+          board[row, i] = Piece.new(color, [row, i])
+        else
+          next if i.odd?
+          board[row, i] = Piece.new(color, [row, i])
+        end
+      end
+    end
+
+    nil
+  end
+
 end
+
+board = Board.new
+
+p board.[]([0, 1])
--- w2d4/checkers/piece.rb
+++ w2d4/checkers/piece.rb
@@ -1,3 +1,94 @@
+require 'byebug'
+
 class Piece
 
+  MOVES = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+  # JUMPING_MOVES = [[]]
+
+  attr_reader :color, :position
+  # May not want this after finished with debugging
+  attr_accessor :king
+
+  def initialize(color, position)
+    @color = color
+    @king = false
+    @position = position
+  end
+
+  def move_diffs
+    # The logic for determining the pieces direction should belong here
+  end
+
+  def maybe_promote
+
+  end
+
+  def perform_jump(pos)
+    jumping_moves = MOVES.map { |x, y| [(x * 2), (y * 2)] }
+
+    jumping_moves.select { |x, y| x.between?(0, 7) && y.between?(0, 7)}
+
+  end
+
+  def perform_slide(pos)
+    if @king && valid_moves_king.include?(pos)
+      @position = pos
+    else
+      raise "Not a valid move!"
+    end
+
+    nil
+  end
+
+  def valid_moves
+    x, y = @position
+    direction = @color == :red ? 1 : -1
+
+    if @king == false
+      valid_moves = MOVES[0..1].map do |dx, dy|
+        [(dx * direction) + x, (dy * direction) + y]
+      end
+    end
+
+    valid_moves.select { |x, y| x.between?(0, 7) && y.between?(0, 7)}
+  end
+
+  def valid_moves_king
+    x, y = @position
+
+    if @king == true
+      valid_moves = MOVES.map do |dx, dy|
+        [dx + x, dy + y]
+      end
+    end
+
+    valid_moves.select { |x, y| x.between?(0, 7) && y.between?(0, 7)}
+  end
+
 end
+
+# red   = Piece.new(:red, [0,1])
+# black = Piece.new(:black, [7,0])
+#
+# # p red.color
+# # p red.position
+# # puts "Valid moves"
+# # p red.valid_moves
+# # p black.color
+# # p black.position
+# # puts "Valid moves"
+# # p black.valid_moves
+#
+# p red.color
+# red.king = true
+# p red.position
+# puts "New position"
+# red.perform_slide([1, 0])
+# p red.position
+# puts
+# p black.color
+# black.king = true
+# p black.position
+# puts "New position"
+# black.perform_slide([6, 1])
+# p black.position
